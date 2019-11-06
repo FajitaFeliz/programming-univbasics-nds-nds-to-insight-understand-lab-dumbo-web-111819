b625750ba9a69236e8668161e184989b71d4d3c6
Ben Miriello <benmiriello@gmail.com>
2019-11-06 22:54:25 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,7 +4,7 @@ require 'pp'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-pp @_db
+# pp @_db
 end
 
-pp directors_database
+# pp directors_database
