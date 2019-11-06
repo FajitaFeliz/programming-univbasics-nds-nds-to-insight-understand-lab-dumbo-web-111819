b63f24f706f7b368ee8ccf86ac69f1010feb5884
Ben Miriello <benmiriello@gmail.com>
2019-11-06 22:23:27 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,5 +10,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  pretty_print_nds(nds)[0][0][0], pretty_print_nds(nds)[0][1][0]
+  # pretty_print_nds(nds)[0][0][0], pretty_print_nds(nds)[0][1][0]
 end
